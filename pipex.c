0b442fd7213dfac5e7e41e29c036b9dd36584c3c
Luceva2002 <luceva2002@gmail.com>
2025-04-14 14:05:08 +0200

    impostata struttura progetto

---
+++ pipex.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luevange <luevange@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 12:10:50 by luevange          #+#    #+#             */
+/*   Updated: 2025/04/14 12:28:08 by luevange         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+void wait_for_children(t_pipex *pipex)
+{
+    
+}
+
+void init_pipex(t_pipex *pipex)
+{
+    
+}
+
+void check_args(int argc)
+{
+    
+}
+
+int main(int argc, char **argv, char **envp)
+{
+    
+}
